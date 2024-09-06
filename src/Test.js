81a44e24e52ffe5df3e5d5831f3fd3c51e667f95
jrme421 <jeromepchallet@gmail.com>
2024-09-06 09:20:02 +0900

    optimization

--- src/Test.js
+++ src/Test.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function SlowComponent() {
   // If this is too slow on your maching, reduce the `length`
-  const words = Array.from({ length: 100_000 }, () => "WORD");
+  const words = Array.from({ length: 50_000 }, () => "WORD");
   return (
     <ul>
       {words.map((word, i) => (
@@ -14,13 +14,28 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
+    </div>
+  );
+}
+
+export default function Test() {
+  //const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>slow counter?!?</h1>
+      <Counter>
+        <SlowComponent />
+      </Counter>
+      {/* <h1>Slow counter?!?</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+      <SlowComponent /> */}
     </div>
   );
 }
